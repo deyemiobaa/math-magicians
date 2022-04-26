5dac46d4d72034838df29f6893876f48fa3f9727
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-26 11:15:05 +0100

    fix bug

--- src/components/quote.js
+++ src/components/quote.js
@@ -5,18 +5,14 @@ class QuoteBody extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'The hardest arithmetic to master is that which enables us to count our blessings',
-      source: 'Eric Hoffer',
+      text: 'Pure mathematics is, in its way, the poetry of logical ideas.',
+      source: 'Albert Einstein',
     };
     this.switchQuote = this.switchQuote.bind(this);
   }
 
   switchQuote() {
     const quotes = [
-      {
-        text: 'The hardest arithmetic to master is that which enables us to count our blessings',
-        source: 'Eric Hoffer',
-      },
       {
         text: 'A man is like a fraction whose numerator is what he is and whose denominator is what he thinks of himself. The larger the denominator, the smaller the fraction.',
         source: 'Leo Tolstoy',
@@ -32,6 +28,10 @@ class QuoteBody extends React.PureComponent {
       {
         text: 'It has become almost a cliché to remark that nobody boasts of ignorance of literature, but it is socially acceptable to boast ignorance of science and proudly claim incompetence in mathematics.',
         source: 'Richard Dawkins',
+      },
+      {
+        text: 'The hardest arithmetic to master is that which enables us to count our blessings',
+        source: 'Eric Hoffer',
       },
     ];
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
