6a8d1570084dca322cf920c6f24c20e4b16efbeb
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-26 11:08:27 +0100

    connect routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import CalculatorBody from './components/calculator';
+import QuoteBody from './components/quote';
+import Home from './components/home';
 
 class AppBody extends React.PureComponent {
   render() {
     return (
       <div className="container">
-        <CalculatorBody />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<CalculatorBody />} />
+          <Route path="/quote" element={<QuoteBody />} />
+        </Routes>
       </div>
     );
   }
